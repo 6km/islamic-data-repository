2b04186d498f9d3c8d2f75b12e700c0f680d4b77
Mohammed <gsft22221@gmail.com>
2025-03-19 06:11:09 +0200

    update Button.svelte

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -4,7 +4,7 @@
 
 <button
 	class={[
-		'cursor-pointer rounded-full bg-blue-100 px-6 pt-2 pb-1 text-cyan-700 transition-colors outline-none select-none hover:bg-blue-200 active:scale-95',
+		'cursor-pointer rounded-full bg-blue-100 px-6 pt-2 pb-1 text-cyan-700 transition-colors outline-none select-none hover:bg-blue-200',
 		class_
 	]}
 	{...props}
