fab96ad6f20cc99d5ff87d6fc84952cda2b8d17d
Mohammed <gsft22221@gmail.com>
2025-03-19 06:11:26 +0200

    create Footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,12 @@
+<footer class="border-t border-t-zinc-300 pt-12 pb-12 text-center text-xl font-semibold">
+	<h4 class="mx-auto w-[36ch] max-w-full leading-9">
+		هذا الموقع مفتوح المصدر على
+		<a
+			class="text-blue-500 hover:underline"
+			href="https://github.com/6km/islamic-data-repository/"
+			target="_blank"
+		>
+			منصة GitHub
+		</a>. يمكنك طلب تعديل، إضافة مصدر، أو حذف أي من المصادر المذكورة في الأعلى.
+	</h4>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Footer from '$lib/components/Footer.svelte';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 	let { children } = $props();
@@ -7,3 +8,5 @@
 <Navbar />
 
 {@render children()}
+
+<Footer />
